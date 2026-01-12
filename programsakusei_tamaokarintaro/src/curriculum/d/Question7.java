d2fa56d24e32ef0c4b60fed0ba5b10e58b4cac7b
user <user@192.168.0.2>
2026-01-12 21:04:49 +0900

    1/12

--- programsakusei_tamaokarintaro/src/curriculum/d/Question7.java
+++ programsakusei_tamaokarintaro/src/curriculum/d/Question7.java
@@ -1,39 +1,39 @@
 package curriculum.d;
 
 public class Question7 {
-    protected String name;
-    protected int hp;
-    protected int at;
-    protected int sp;
+	private String name;
+	private int hp;
+	private int at;
+	private int sp;
 
-    public Question7(String name, int hp, int at, int sp) {
-        this.name = name;
-        this.hp = hp;
-        this.at = at;
-        this.sp = sp;
-    }
+	public Question7(String name, int hp, int at, int sp) {
+		this.name = name;
+		this.hp = hp;
+		this.at = at;
+		this.sp = sp;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getHp() {
-        return hp;
-    }
+	public int getHp() {
+		return hp;
+	}
 
-    public int getAt() {
-        return at;
-    }
+	public int getAt() {
+		return at;
+	}
 
-    public int getSp() {
-        return sp;
-    }
+	public int getSp() {
+		return sp;
+	}
 
-    public void takeDamage(int damage) {
-        hp -= damage;
-    }
+	public void takeDamage(int damage) {
+		hp -= damage;
+	}
 
-    public boolean isAlive() {
-        return hp > 0;
-    }
+	public boolean isAlive() {
+		return hp > 0;
+	}
 }
